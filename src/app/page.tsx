fb06d193d5a9a830749d5cad485ec09ca1b5b7a6
jaebungs <jaebungs@gmail.com>
2025-07-02 19:57:52 -0400

    implement edit and preview button on the EditorNav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+'use client'
+import { useState } from 'react'
+import EditorNav from '@/app/components/EditorNav'
 import EmailInput from "@/app/formComponents/EmailInput"
 import ShortAnswerInput from "@/app/formComponents/ShortAnswerInput"
+import { FormMode } from "@/app/types/formMode"
 
 export default function Home() {
+  const [currentMode, setCurrentMode] = useState<FormMode>('edit')
+
+  const handleModeChange = (mode: FormMode) => {
+    setCurrentMode(mode)
+  }
   return (
     <div className="">
-      <div>
-        <EmailInput text="Email" label="Email" placeholder="Enter your email" formMode='edit' />
+      <EditorNav handleModeChange={handleModeChange} />
+      <div className="w-96">
+        <EmailInput text="Email" label="Email" placeholder="Enter your email" formMode={currentMode} />
         <ShortAnswerInput text="Short Answer" label="Short Answer" placeholder="Enter your short answer" />
       </div>
     </div>
