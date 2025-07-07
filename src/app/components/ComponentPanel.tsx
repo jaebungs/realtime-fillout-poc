43d7337f5095f005db96ba99a99f6a7f90fc3cd5
jaebungs <jaebungs@gmail.com>
2025-07-06 21:06:55 -0400

    create side ComponentPanel component skeleton

---
+++ src/app/components/ComponentPanel.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+const ComponentPanel = () => {
+
+    return (
+        <div className="w-full flex flex-col justify-between h-full max-w-[226px] min-w-[180px] lg:max-w-[300px] lg:min-w-[270px]">
+            <div className="flex h-screen flex-shrink-0 duration-200 bg-gray-50 border-x-[0.5px] border-gray-300 w-[226px] lg:w-[300px]">
+
+            </div>
+        </div>
+    )
+}
+
+export default ComponentPanel
