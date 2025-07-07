8505023d3a4907cb7751f3ed923dc20420ea174a
jaebungs <jaebungs@gmail.com>
2025-07-07 19:21:50 -0400

    add edit canvas area

--- src/app/components/EditCanvas.tsx
+++ src/app/components/EditCanvas.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import EmailInput from "@/app/formComponents/EmailInput"
 import ShortAnswerInput from "@/app/formComponents/ShortAnswerInput"
+import { renderComponent } from "@/app/utils/renderComponent" 
 import { useFormStore } from '@/app/store/formStore'
 
 const EditCanvas = () => {
+    const formComponents = useFormStore(state => state.formComponents)
   const formMode = useFormStore(state => state.formMode)
+//   const { addFormComponent } = useFormStore
+
+  const onDragStart = (e:  React.DragEvent<HTMLDivElement>) => {
+    console.log(e)
+  }
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+
+  }
 
   return (
     <div className="w-96 sm:pb-4 flex items-center flex-col h-full gap-1">
-        <EmailInput text="Email" label="Email" placeholder="Enter your email" formMode={formMode} />
-        <ShortAnswerInput text="Short Answer" ariaLabel="Short Answer" placeholder="Enter your short answer" formMode={formMode} />
+      { 
+        formComponents.map((form, index) => {
+          return <div key={form.id}>{renderComponent(form)}</div>
+        })
+      }
   </div>
   )
 }
