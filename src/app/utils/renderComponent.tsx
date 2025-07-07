d48a8e314a82e75d01d7de027680af00a79bb18a
jaebungs <jaebungs@gmail.com>
2025-07-07 19:21:13 -0400

    add form component renderer util

---
+++ src/app/utils/renderComponent.tsx
@@ -0,0 +1,28 @@
+import { FormComponent } from '@/app/types/formComponent'
+import EmailInput from "@/app/formComponents/EmailInput"
+import ShortAnswerInput from "@/app/formComponents/ShortAnswerInput"
+import { useFormStore } from '@/app/store/formStore'
+import { FormMode } from '@/app/types/formMode'
+
+export const renderComponent = (component: FormComponent) => {
+    const formMode = useFormStore(state => state.formMode)
+    
+    switch(component.componentName) {
+        case 'EmailInput':
+            return <EmailInput 
+                text="Email" 
+                label="Email" 
+                placeholder="Enter your email" 
+                formMode={formMode} 
+            />
+        case 'ShortAnswerInput':
+            return <ShortAnswerInput 
+                text="Short Answer" 
+                ariaLabel="Short Answer" 
+                placeholder="Enter your short answer" 
+                formMode={formMode} 
+            />
+        default:
+            return null
+    }
+}
