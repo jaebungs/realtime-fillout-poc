47d1b73392cdab67d32c687ede4a42816a345e78
jaebungs <jaebungs@gmail.com>
2025-07-06 21:02:59 -0400

    use useFormStore for mode change

--- src/app/components/EditorNav.tsx
+++ src/app/components/EditorNav.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { FormMode } from '@/app/types/formMode'
+import { useFormStore } from '@/app/store/formStore'
 
-interface EditorNavProps {
-  handleModeChange: (mode: FormMode) => void
-}
-
-const EditorNav = ({handleModeChange}: EditorNavProps) => {
+const EditorNav = () => {
+    const { changeFormMode } = useFormStore()
   return (
     <div className='pl-2 flex items-center w-full justify-between'>
         <div className='font-medium flex items-center py-[11px] md:min-w-[310px] largeXl:min-w-[354px] largeXlCustom:min-w-[424px]'>
@@ -16,12 +13,12 @@ const EditorNav = ({handleModeChange}: EditorNavProps) => {
         </div>
         <div className='flex justify-end items-center py-[11px] pr-7 gap-4 md:min-w-[310px] largeXl:min-w-[354px] largeXlCustom:min-w-[424px]'>
             <button className='inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition focus-visible:outline-none focus-visible:ring-2 disabled:opacity-60 [&_svg]:pointer-events-none [&_svg]:shrink-0 select-none disabled:cursor-disabled -tracking-[0.01rem] border border-gray-200 shadow-sm hover:bg-gray-50 focus-visible:ring-black/30 bg-white text-[#1a1a1a] h-9 rounded-lg px-4 py-2'
-                onClick={() => handleModeChange('edit')}
+                onClick={() => changeFormMode('edit')}
             >
                 Edit
             </button>
             <button className='inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition focus-visible:outline-none focus-visible:ring-2 disabled:opacity-60 [&_svg]:pointer-events-none [&_svg]:shrink-0 select-none disabled:cursor-disabled -tracking-[0.01rem] border border-gray-200 shadow-sm hover:bg-gray-50 focus-visible:ring-black/30 bg-white text-[#1a1a1a] h-9 rounded-lg px-4 py-2'
-                onClick={() => handleModeChange('preview')}
+                onClick={() => changeFormMode('preview')}
             >
                 Preview
             </button>
