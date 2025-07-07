74a225e19fb0009472f304786ce8a128a080cb66
jaebungs <jaebungs@gmail.com>
2025-07-06 21:02:19 -0400

    use devtool

--- src/app/store/formStore.ts
+++ src/app/store/formStore.ts
@@ -1,4 +1,6 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { FormMode } from '@/app/types/formMode'
 
 interface FormComponent {
     id: string,
@@ -8,21 +10,31 @@ interface FormComponent {
 
 interface FormStore {
     formComponents: FormComponent[]
+    formMode: FormMode,
+    changeFormMode: (mode: FormMode) => void
     addFormComponent: (component: FormComponent) => void
     removeFormComponent: (id: string) => void
     clearFormComponents: () => void
 }
 
-export const useFormStore = create<FormStore>((set, get) => ({
-    formComponents: [],
-    
-    addFormComponent: (component) => set((state) => ({
-        formComponents: [...state.formComponents, component].sort((a, b) => a.order - b.order)
-    })),
-    
-    removeFormComponent: (id) => set((state) => ({
-        formComponents: state.formComponents.filter(comp => comp.id !== id)
-    })),
-    
-    clearFormComponents: () => set({ formComponents: [] })
-}))
+export const useFormStore = create<FormStore>()(
+  devtools(
+    (set, get) => ({
+        formComponents: [],
+        formMode: 'edit',
+        changeFormMode: (mode) => set({ formMode: mode }),
+        addFormComponent: (component) => set((state) => ({
+            formComponents: [...state.formComponents, component].sort((a, b) => a.order - b.order)
+        })),
+        
+        removeFormComponent: (id) => set((state) => ({
+            formComponents: state.formComponents.filter(comp => comp.id !== id)
+        })),
+        
+        clearFormComponents: () => set({ formComponents: [] })
+    }),
+    {
+      name: 'form-store',
+    }
+  )
+)
