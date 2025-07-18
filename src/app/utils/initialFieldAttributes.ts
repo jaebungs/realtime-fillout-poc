d42af9fa6e51ec7bdc87ca91553c5cc27472d5fb
jaebungs <jaebungs@gmail.com>
2025-07-18 17:40:55 -0400

    add initial attributes of the form components

---
+++ src/app/utils/initialFieldAttributes.ts
@@ -0,0 +1,15 @@
+// Specify the default attributes for each form field component below
+const initialFieldAttributes = {
+    'EmailInput': {
+        text: 'Email',
+        ariaLabel: 'email-input',
+        placeholder: '',
+    },
+    'ShortAnswerInput': {
+        text: 'Type your question here',
+        ariaLabel: 'short-text-input',
+        placeholder: '',
+    }
+}
+
+export default initialFieldAttributes
