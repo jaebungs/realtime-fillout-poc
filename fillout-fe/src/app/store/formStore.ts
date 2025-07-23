96956f2220f1c6a36fcdaae5cfbe3e899f3f456f
jaebungs <jaebungs@gmail.com>
2025-07-22 23:12:02 -0400

    comment non websocket code

--- fillout-fe/src/app/store/formStore.ts
+++ fillout-fe/src/app/store/formStore.ts
@@ -20,6 +20,10 @@ interface FormStore {
     updateComponentProperty: (componentId: string, property: keyof FormComponent, value: any) => void
 }
 
+/**
+ * The commented code below is for FE only fillout, using store before websocket was implemented
+ * Uncomment it and comment ws related code to make non-realtime FE work
+ */
 export const useFormStore = create<FormStore>()(
   devtools(
     (set, get) => ({
@@ -29,82 +33,82 @@ export const useFormStore = create<FormStore>()(
         changeFormMode: (mode) => set({ formMode: mode }),
         selectComponent: (component) => set({ selectedComponent: component }),
         addFormComponent: (name, order) => set((state) => {
-            const component = {
-                id: uuidv4(),
-                order: order,
-                componentName: name,
-                ...initialFieldAttributes[name]
-            }
+            // const component = {
+            //     id: uuidv4(),
+            //     order: order,
+            //     componentName: name,
+            //     ...initialFieldAttributes[name]
+            // }
 
-            const newFormComponents = [...state.formComponents, component].sort((a, b) => a.order - b.order)
+            // const newFormComponents = [...state.formComponents, component].sort((a, b) => a.order - b.order)
             
-            // WS migration
             ws.send(JSON.stringify({
                 type: 'addFormComponent',
                 componentName: name,
                 order: 'last'
             }))
-            return {formComponents : newFormComponents}
+            // return {formComponents : newFormComponents}
+            return {}
         }),
         addFormComponentAtPosition: (name, position) => set((state) => {
-            const component = {
-                id: uuidv4(),
-                order: position,
-                componentName: name,
-                ...initialFieldAttributes[name]
-            }
+            // const component = {
+            //     id: uuidv4(),
+            //     order: position,
+            //     componentName: name,
+            //     ...initialFieldAttributes[name]
+            // }
 
-            // Insert the component at the specified position
-            const newFormComponents = [...state.formComponents]
-            newFormComponents.splice(position, 0, component)
+            // // Insert the component at the specified position
+            // const newFormComponents = [...state.formComponents]
+            // newFormComponents.splice(position, 0, component)
             
-            // Update the order of all components after the insertion point
-            for (let i = position + 1; i < newFormComponents.length; i++) {
-                newFormComponents[i].order = i
-            }
+            // // Update the order of all components after the insertion point
+            // for (let i = position + 1; i < newFormComponents.length; i++) {
+            //     newFormComponents[i].order = i
+            // }
             
-            // WS migration
             ws.send(JSON.stringify({
                 type: 'addFormComponentAtPosition',
                 componentName: name,
                 order: position
             }))
-            return { formComponents: newFormComponents }
+            // return { formComponents: newFormComponents }
+            return {}
         }),
         changeFormOrder: (draggedComponent, dropTargetComponent) => set(state => {
-            if (!draggedComponent) return state
-            console.log(draggedComponent.order, dropTargetComponent.order)
-            if (draggedComponent.order === dropTargetComponent.order) return state
+            // if (!draggedComponent) return state
+            // console.log(draggedComponent.order, dropTargetComponent.order)
+            // if (draggedComponent.order === dropTargetComponent.order) return state
 
-            const dragComponentPosition = draggedComponent.order
-            const targetPosition = dropTargetComponent.order
-            // move the dragging component to the new position
-            const filteredComponents = state.formComponents.filter(form => form.id !== draggedComponent.id)
-            const newComponents = [...filteredComponents]
-            newComponents.splice(targetPosition, 0, { ...draggedComponent, order: targetPosition })
+            // const dragComponentPosition = draggedComponent.order
+            // const targetPosition = dropTargetComponent.order
+            // // move the dragging component to the new position
+            // const filteredComponents = state.formComponents.filter(form => form.id !== draggedComponent.id)
+            // const newComponents = [...filteredComponents]
+            // newComponents.splice(targetPosition, 0, { ...draggedComponent, order: targetPosition })
 
-            // update the order property of the component that is being replaced
-            const replacedComponent = newComponents.find(form => form.id === dropTargetComponent.id)
-            if (replacedComponent) {
-                replacedComponent.order = dragComponentPosition
-            }
+            // // update the order property of the component that is being replaced
+            // const replacedComponent = newComponents.find(form => form.id === dropTargetComponent.id)
+            // if (replacedComponent) {
+            //     replacedComponent.order = dragComponentPosition
+            // }
 
-            // WS migration
             ws.send(JSON.stringify({
                 type: 'changeFormOrder',
                 draggedComponent,
                 dropTargetComponent
             }))
-            return { formComponents: newComponents }
+            // return { formComponents: newComponents }
+            return {}
         }),
         removeFormComponent: (form) => set((state) => {
-            const newFormComponents = state.formComponents.filter(comp => comp.id !== form.id)
+            // const newFormComponents = state.formComponents.filter(comp => comp.id !== form.id)
 
-            // Update the order of remaining components to maintain proper sequence
-            const updatedComponents = newFormComponents.map((comp, index) => ({
-                ...comp,
-                order: index
-            }))
+            // // Update the order of remaining components to maintain proper sequence
+            // const updatedComponents = newFormComponents.map((comp, index) => ({
+            //     ...comp,
+            //     order: index
+            // }))
 
             // WS migration
             ws.send(JSON.stringify({
@@ -112,10 +116,11 @@ export const useFormStore = create<FormStore>()(
                 targetComponent: form
             }))
             
-            return {
-                formComponents: updatedComponents,
-                selectedComponent: null
-            }
+            // return {
+            //     formComponents: updatedComponents,
+            //     selectedComponent: null
+            // }
+            return {}
         }),
         
         clearFormComponents: () => set({ formComponents: [] }),
