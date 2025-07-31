c2f4a6ec5dd83531def34ccaa7ba2732665575f2
jaebungs <jaebungs@gmail.com>
2025-07-30 22:38:07 -0400

    change jest setup

--- fillout-fe/jest.config.js
+++ fillout-fe/jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 
 // Add any custom config to be passed to Jest
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: [],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
